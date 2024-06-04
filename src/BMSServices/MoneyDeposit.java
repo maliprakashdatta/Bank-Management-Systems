3c68537704f4f3cb50e3c5ed31333aefac11163e
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-06-04 14:33:55 +0530

    Money Deposit

--- src/BMSServices/MoneyDeposit.java
+++ src/BMSServices/MoneyDeposit.java
@@ -1,4 +1,19 @@
 package BMSServices;
 
-public class MoneyDeposit {
+import BMSModel.Bank;
+
+import java.util.Scanner;
+
+public class MoneyDeposit extends BankMain implements Bank {
+    //method to deposit money
+    public void deposit() {
+        long amt;
+        long balance = 0;
+        System.out.println("Enter the amount you want to deposit: ");
+        Scanner scanner = new Scanner(System.in);
+        amt = scanner.nextLong();
+        balance = balance + amt;
+        System.out.println("Deposit is Successfully "+amt+"/-"+" Rupay");
+
+    }
 }
