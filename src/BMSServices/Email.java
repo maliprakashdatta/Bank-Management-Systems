bdce1e913144dc2ac49960b78fc8ea9b14bd7d1a
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-06-06 19:42:19 +0530

    Email sending service Information

---
+++ src/BMSServices/Email.java
@@ -0,0 +1,39 @@
+package BMSServices;
+
+import BMSModel.Bank;
+
+public abstract class Email extends BankMain implements Bank {
+   private  String to,cc,subject, body;
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getCc() {
+        return cc;
+    }
+
+    public void setCc(String cc) {
+        this.cc = cc;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
